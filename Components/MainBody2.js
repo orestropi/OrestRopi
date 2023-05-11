70f299f8d48ad1c5c419794ba48e2c0c9ed5c3e5
orestropi <oropi@wpi.edu>
2023-05-11 04:07:28 -0400

    fcr

--- Components/MainBody2.js
+++ Components/MainBody2.js
@@ -1,32 +1,168 @@
-import { Box, Card, Grid, Paper, Typography } from '@mui/material'
+import { Card, Grid, Paper, Typography } from '@mui/material'
 import React from 'react'
 
 
 function MainBody2() {
 
     return (
-        <div >
+        <div>
             <div className='main-body-item' >
-                <Grid direction="column" container spacing={10}style={{ marginTop: '1%'}}>
-
-
-                    <Grid item direction="row" height={'120vh'} style={{ marginBottom: '10%'}} container spacing={2}>
-                        
-                        <Grid  style={{ justifyContent: "flex-start" }}>
-                        
-                        <h1 style={{ marginBottom: '1%', textAlign: 'center' }}>Chronicle in Stone Essay</h1>
                 
+                <Grid direction="row" container spacing={4}style={{ marginTop: '5%', marginBottom: '5%'}}>
                         
-                        <object data="https://cisorestessay.tiiny.site/" type="application/pdf" width="350%" height="100%"></object>
+                        <Grid item xs={12} sm={12} md={12} lg={6}>
+                        <p>
+                        <img
+                            src="./GridGame.png"
+                            alt="It's me!"
+                        />
+                        </p>
 
+                        <p>My name is Orest Ropi and I am a part of WPI's class of 2023.</p>
+                        <p>I graduated for computer science in December, 2022.</p>
+                        <p>
+                        <a href="https://github.com/orestropi"> Github</a> |
+                        <a href="https://www.linkedin.com/in/orest-ropi-480036231/"> Linkedin</a>
+                        </p>
                         </Grid>
-                        
-                    </Grid>
 
-                </Grid>
-            </div>
-        </div>
-    )
-}
+                        <Grid item xs={12} sm={12} md={12} lg={6}>
+                        <h2>Computer Science Courses taken at WPI</h2>
+
+                        <p>
+                        <a style={{ color: 'deepskyblue'}} href="https://www.wpi.edu/academics/departments/computer-science/courses">
+                            Link here to learn more about my courses ...</a
+                        ><br />
+                        CS 1101 INTRO TO PROGRAM DESIGN <br />
+                        CS 2102 OBJCT-ORIENTED DESIGN CONCEPTS <br />
+                        CS 2011 INTRO-MACHINE ORG & ASSEMBLY <br />
+                        CS 2022 DISCRETE MATHEMATICS <br />
+                        CS 2223 ALGORITHMS <br />
+                        CS 2303 SYSTEMS PROGRAMMING CONCEPTS <br />
+                        CS 3013 OPERATING SYSTEMS <br />
+                        CS 3043 SOCIAL IMPL OF INFO PROCESSING <br />
+                        CS 3133 FOUNDATIONS OF COMP. SCIENCE <br />
+                        CS 3431 DATABASE SYSTEMS <br />
+                        CS 3516 COMPUTER NETWORKS <br />
+                        CS 3733 SOFTWARE ENGINEERING <br />
+                        CS 4032 NUMERICAL METHODS FOR LINEAR AND NONLINEAR SYSTEMS <br />
+                        CS 4241 WEBWARE <br />
+                        CS 4341 INTRO TO AI <br />
+                        CS 480X DATA VISUALIZATION <br />
+                        CS 4432 DATABASE SYSTEMS II <br />
+                        </p>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12} lg={6} >
+
+                        <h4>Experience Programing Languages:</h4>
+
+                        <p>
+                        Java - a lot <br />
+                        HTML - some <br />
+                        JavaScript - some <br />
+                        C++ - some <br />
+                        Python - some <br />
+                        MATLAB - some <br />
+                        CSS - a little <br />
+                        C - a little <br />
+                        LaTeX - a little <br />
+                        R - a little <br />
+                        Unit testing - a lot <br />
+                        </p>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12} lg={6} >
+
+                        <h4>Experience Technologies:</h4>
+                        <p>
+                        Windows and Linux - a lot <br />
+                        Git - a lot <br />
+                        Node.js - some <br />
+                        Express.js - some <br />
+                        Mongo DB - some <br />
+                        Oracle DB - some <br />
+                        D3.js - some <br />
+                        MySQL DB - a litte <br />
+                        AWS - a little <br />
+                        Swing - a little <br />
+                        react.js - a little <br />
+                        </p>
+                                            
+                                            </Grid>
+                                    </Grid>
+                                </div>
+                            </div>
+                        )
+                    }
 
 export default MainBody2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* 
+
+// import { Box, Card, Grid, Paper, Typography } from '@mui/material'
+// import React from 'react'
+
+
+// function MainBody2() {
+
+//     return (
+//         <div >
+//             <div className='main-body-item' >
+//                 <Grid direction="column" container spacing={10}style={{ marginTop: '1%'}}>
+
+
+//                     <Grid item direction="row" height={'120vh'} style={{ marginBottom: '10%'}} container spacing={2}>
+                        
+//                         <Grid  style={{ justifyContent: "flex-start" }}>
+                        
+//                         <h1 style={{ marginBottom: '1%', textAlign: 'center' }}>Chronicle in Stone Essay</h1>
+                
+                        
+//                         <object data="https://cisorestessay.tiiny.site/" type="application/pdf" width="350%" height="100%"></object>
+
+//                         </Grid>
+                        
+//                     </Grid>
+
+//                 </Grid>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default MainBody2 */}
--- Components/MainBody3.js
+++ Components/MainBody3.js
@@ -1,180 +1,261 @@
-import { Card, Grid, Paper, Typography, ImageList, ImageListItem } from '@mui/material'
+
+import { Card, Grid, Paper, Typography } from '@mui/material'
 import React from 'react'
 
-function srcset(image, size, rows = 1, cols = 1) {
-    return {
-      src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-      srcSet: `${image}?w=${size * cols}&h=${
-        size * rows
-      }&fit=crop&auto=format&dpr=2 2x`,
-    };
-  }
 
-  
 function MainBody3() {
 
     return (
-        <div >
+        <div>
             <div className='main-body-item' >
-                <Grid direction="column" container spacing={10}style={{ marginTop: '1%'}}>
-                <Grid item direction="row" height={'100vh'} style={{ marginBottom: '1%'}} container spacing={2}>
-                        
-                        <Grid  style={{ justifyContent: "flex-start" }}>
-                        
-                        <h1 style={{ marginBottom: '1%', textAlign: 'center' }}>Quotes about Gjirokastra, Albania - "City of Stone"</h1>
                 
-                        <body style={{ marginBottom: '5%', textAlign: 'center' }}>
+                <Grid direction="row" container spacing={4}style={{ marginTop: '5%', marginBottom: '5%'}}>
                         
+                        <Grid item xs={12} sm={12} md={12} >
+                            <h2>Contact</h2>
+                            
+                        
+                        <li>Number: 857-928-3387</li>
+                        <li>Email:  oropi@wpi.edu</li>
+                        <li>LinkedIn: <a style={{ color: 'deepskyblue'}} href="https://www.linkedin.com/in/orest-ropi-480036231/">
+                            Link</a
+                        ></li>
+                        <li>Uses The Model View Controller (MVC) design pattern for scalability and development efficiency.</li>
                         <div>
-                        <img style={{ marginBottom: '3%'}}src={`./kadb.jpg`}/></div>
-                        
-                        "Everything in the city was old and made of stone, from the streets and fountains to the roofs of the sprawling age-old houses covered with grey slates like gigantic scales." (Kadare 20)
-
-                        <br></br><br></br>"The city rejected all comparisons. In fact, it looked like nothing else. It could no more support comparison than it would allow rain, hail, rainbows, or multicoloured foreign flags to remain for long on its rooftops, for they were as fleeting and unreal as the city was lasting and anchored in solid matter." (Kadare 20)
-
-
-                        <br></br><br></br>"It was a slanted city, set at a sharper angle than perhaps any other city on earth, and it defied the laws of architecture and city planning. The top of one house might graze the foundation of another, and it was surely the only place in the world where if you slipped and fell in the street, you might well land on the roof of a house — a peculiarity known most intimately to drunks." (Kadare 20)
-
-                        <br></br><br></br>"He looked out at the grey
-                    cliffs. The city’s numberless roofs were barely visible in the
-                    fog."(Kadare 178)
-                    </body>
-
+                        <iframe scrolling="no" id='iframe-chart' width="100%" height="650vh" src="https://rickandmortyfinalp.glitch.me/" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </div>
                         </Grid>
-                        
-                    </Grid>
-
-                    <Grid item direction="row" height={'120vh'} style={{ marginBottom: '10%'}} container spacing={2}>
-                        
-                        <Grid  style={{ justifyContent: "flex-start" }}>
-                        
-                        <h1 style={{ marginBottom: '1%', textAlign: 'center' }}>Pictures of Gjirokastra, Albania - "City of Stone"</h1>
-                
-                        <ImageList
-                        sx={{ width: 1000, height: 900 }}
-                        variant="quilted"
-                        cols={4}
-                        rowHeight={242}
-                        >
-                        {itemData.map((item) => (
-                            <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-                            <img
-                                {...srcset(item.img, 121, item.rows, item.cols)}
-                                alt={item.title}
-                                loading="lazy"
-                            />
-                            </ImageListItem>
-                        ))}
-                        </ImageList>
-                        </Grid>
-                        
-                    </Grid>
-
-                    <Grid item direction="row" height={'120vh'} style={{ marginBottom: '10%'}} container spacing={2}>
-                        
-                        <Grid  style={{ justifyContent: "flex-start" }}>
-                        
-                        <h1 style={{ marginBottom: '1%', textAlign: 'center' }}>Albania - Communism Recovery Visualized</h1>
-                
-                        <ImageList
-                        sx={{ width: 1000, height: 900 }}
-                        variant="quilted"
-                        cols={4}
-                        rowHeight={242}
-                        >
-                        {itemData2.map((item) => (
-                            <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-                            <img
-                                {...srcset(item.img, 121, item.rows, item.cols)}
-                                alt={item.title}
-                                loading="lazy"
-                            />
-                            </ImageListItem>
-                        ))}
-                        </ImageList>
-                        </Grid>
-                        
-                    </Grid>
-
                 </Grid>
             </div>
         </div>
     )
 }
 
-const itemData = [
-    {
-      img: 'AGmap.svg',
-      title: 'Breakfast',
-      rows: 3,
-      cols: 2,
-    },
-    {
-      img: 'download.jpg',
-      title: 'Burger',
-    },
-    {
-      img: 'images.jpg',
-      title: 'Camera',
-    },
-    {
-      img: 'gjiro1.jpg',
-      title: 'Coffee',
-      cols: 2,
-    },
-    {
-      img: 'gjirokaster-albania-22.jpg',
-      title: 'Hats',
-      cols: 2,
-    },
-    {
-      img: 'Gjirokaster.jpg',
-      title: 'Honey',
-      rows: 2,
-      cols: 2,
-    },
-    {
-      img: 'top.jpg',
-      title: 'Basketball',
-      rows: 2,
-      cols: 2,
-    },
-    
-  ]
-
-  const itemData2 = [
-    {
-      img: 'chart.svg',
-      title: 'Basketball',
-      rows: 2.1,
-      cols: 4,
-    },
-    {
-        img: 'chart3.svg',
-        title: 'Basketball',
-        rows: 2.1,
-      cols: 4,
-      },
-    {
-        img: 'chart4.svg',
-        title: 'Basketball',
-        rows: 2.1,
-      cols: 4,
-      },
-    {
-        img: 'chart5.svg',
-        title: 'Basketball',
-        rows: 2.1,
-      cols: 4,
-      },
-      {
-        img: 'chart6.svg',
-        title: 'Basketball',
-        rows: 2.1,
-      cols: 4,
-      },
-    
-    
-  ]
-
 export default MainBody3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Card, Grid, Paper, Typography, ImageList, ImageListItem } from '@mui/material'
+// import React from 'react'
+
+// function srcset(image, size, rows = 1, cols = 1) {
+//     return {
+//       src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+//       srcSet: `${image}?w=${size * cols}&h=${
+//         size * rows
+//       }&fit=crop&auto=format&dpr=2 2x`,
+//     };
+//   }
+
+  
+// function MainBody3() {
+
+//     return (
+//         <div >
+//             <div className='main-body-item' >
+//                 <Grid direction="column" container spacing={10}style={{ marginTop: '1%'}}>
+//                 <Grid item direction="row" height={'100vh'} style={{ marginBottom: '1%'}} container spacing={2}>
+                        
+//                         <Grid  style={{ justifyContent: "flex-start" }}>
+                        
+//                         <h1 style={{ marginBottom: '1%', textAlign: 'center' }}>Quotes about Gjirokastra, Albania - "City of Stone"</h1>
+                
+//                         <body style={{ marginBottom: '5%', textAlign: 'center' }}>
+                        
+//                         <div>
+//                         <img style={{ marginBottom: '3%'}}src={`./kadb.jpg`}/></div>
+                        
+//                         "Everything in the city was old and made of stone, from the streets and fountains to the roofs of the sprawling age-old houses covered with grey slates like gigantic scales." (Kadare 20)
+
+//                         <br></br><br></br>"The city rejected all comparisons. In fact, it looked like nothing else. It could no more support comparison than it would allow rain, hail, rainbows, or multicoloured foreign flags to remain for long on its rooftops, for they were as fleeting and unreal as the city was lasting and anchored in solid matter." (Kadare 20)
+
+
+//                         <br></br><br></br>"It was a slanted city, set at a sharper angle than perhaps any other city on earth, and it defied the laws of architecture and city planning. The top of one house might graze the foundation of another, and it was surely the only place in the world where if you slipped and fell in the street, you might well land on the roof of a house — a peculiarity known most intimately to drunks." (Kadare 20)
+
+//                         <br></br><br></br>"He looked out at the grey
+//                     cliffs. The city’s numberless roofs were barely visible in the
+//                     fog."(Kadare 178)
+//                     </body>
+
+//                         </Grid>
+                        
+//                     </Grid>
+
+//                     <Grid item direction="row" height={'120vh'} style={{ marginBottom: '10%'}} container spacing={2}>
+                        
+//                         <Grid  style={{ justifyContent: "flex-start" }}>
+                        
+//                         <h1 style={{ marginBottom: '1%', textAlign: 'center' }}>Pictures of Gjirokastra, Albania - "City of Stone"</h1>
+                
+//                         <ImageList
+//                         sx={{ width: 1000, height: 900 }}
+//                         variant="quilted"
+//                         cols={4}
+//                         rowHeight={242}
+//                         >
+//                         {itemData.map((item) => (
+//                             <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+//                             <img
+//                                 {...srcset(item.img, 121, item.rows, item.cols)}
+//                                 alt={item.title}
+//                                 loading="lazy"
+//                             />
+//                             </ImageListItem>
+//                         ))}
+//                         </ImageList>
+//                         </Grid>
+                        
+//                     </Grid>
+
+//                     <Grid item direction="row" height={'120vh'} style={{ marginBottom: '10%'}} container spacing={2}>
+                        
+//                         <Grid  style={{ justifyContent: "flex-start" }}>
+                        
+//                         <h1 style={{ marginBottom: '1%', textAlign: 'center' }}>Albania - Communism Recovery Visualized</h1>
+                
+//                         <ImageList
+//                         sx={{ width: 1000, height: 900 }}
+//                         variant="quilted"
+//                         cols={4}
+//                         rowHeight={242}
+//                         >
+//                         {itemData2.map((item) => (
+//                             <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+//                             <img
+//                                 {...srcset(item.img, 121, item.rows, item.cols)}
+//                                 alt={item.title}
+//                                 loading="lazy"
+//                             />
+//                             </ImageListItem>
+//                         ))}
+//                         </ImageList>
+//                         </Grid>
+                        
+//                     </Grid>
+
+//                 </Grid>
+//             </div>
+//         </div>
+//     )
+// }
+
+// const itemData = [
+//     {
+//       img: 'AGmap.svg',
+//       title: 'Breakfast',
+//       rows: 3,
+//       cols: 2,
+//     },
+//     {
+//       img: 'download.jpg',
+//       title: 'Burger',
+//     },
+//     {
+//       img: 'images.jpg',
+//       title: 'Camera',
+//     },
+//     {
+//       img: 'gjiro1.jpg',
+//       title: 'Coffee',
+//       cols: 2,
+//     },
+//     {
+//       img: 'gjirokaster-albania-22.jpg',
+//       title: 'Hats',
+//       cols: 2,
+//     },
+//     {
+//       img: 'Gjirokaster.jpg',
+//       title: 'Honey',
+//       rows: 2,
+//       cols: 2,
+//     },
+//     {
+//       img: 'top.jpg',
+//       title: 'Basketball',
+//       rows: 2,
+//       cols: 2,
+//     },
+    
+//   ]
+
+//   const itemData2 = [
+//     {
+//       img: 'chart.svg',
+//       title: 'Basketball',
+//       rows: 2.1,
+//       cols: 4,
+//     },
+//     {
+//         img: 'chart3.svg',
+//         title: 'Basketball',
+//         rows: 2.1,
+//       cols: 4,
+//       },
+//     {
+//         img: 'chart4.svg',
+//         title: 'Basketball',
+//         rows: 2.1,
+//       cols: 4,
+//       },
+//     {
+//         img: 'chart5.svg',
+//         title: 'Basketball',
+//         rows: 2.1,
+//       cols: 4,
+//       },
+//       {
+//         img: 'chart6.svg',
+//         title: 'Basketball',
+//         rows: 2.1,
+//       cols: 4,
+//       },
+    
+    
+//   ]
+
+// export default MainBody3
