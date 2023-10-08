fe482905ca9d402cd7bdcb0f2f098b8584bd619c
Orest Ropi <73619173+orestropi@users.noreply.github.com>
2023-10-07 23:49:18 -0400

    Update MainFooter.js

--- Components/MainFooter.js
+++ Components/MainFooter.js
@@ -67,6 +67,8 @@ function MainFooter() {
                                 </Typography>
                                 <Typography variant="subtitle1" color="white" >
                                     <a>Software Engineer Internship - Schneider Electric [4 Months]</a>
+                                </Typography>
+                                <Typography variant="subtitle1" color="white" >
                                     <a>\n Full-Stack Developer Internship - Diamond Diagnostics [2 Months]</a>
                                 </Typography>
                             </Grid>
